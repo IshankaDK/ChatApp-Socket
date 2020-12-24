58aeda4115a9ea0ca6aeafdb09ac0922cd93fc79
IshankaDK <idkodippili@gmail.com>
2020-12-24 20:32:59 +0530

    first commit

---
+++ src/controller/ClientLogin.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class ClientLogin {
+    public void txtUserName(ActionEvent actionEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/controller/ServerController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class ServerController {
+    public TextArea txtArea;
+
+    public void btnStartOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStopOnAction(ActionEvent actionEvent) {
+    }
+}
